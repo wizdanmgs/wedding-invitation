4ad09f50ad41babdb7a6788a4d707ef836dde161
Wizdan M. G. S. <wizdanshodri@protonmail.com>
2024-10-14 02:12:52 +0700

    Send message

--- wedding_invitation/app/views.py
+++ wedding_invitation/app/views.py
@@ -1,4 +1,7 @@
+import json
+
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Message
 
 
@@ -9,3 +12,17 @@ def index(request):
         "messages": Message.objects.all(),
     }
     return render(request, "index.html", context)
+
+
+def message(request):
+    context = {}
+    if request.method == "POST":
+        name = request.POST["name"]
+        message = request.POST["message"]
+        Message.objects.create(name=name, message=message)
+        context = {
+            "name": name,
+            "message": message,
+        }
+
+        return JsonResponse(context)
